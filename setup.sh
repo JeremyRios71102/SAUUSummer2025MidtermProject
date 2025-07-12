db0c393b6612b99d481b821fa6b9214abe957f93
stoffletd <156819575+stoffletd@users.noreply.github.com>
2025-07-12 11:44:21 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Named Pipe
+mkfifo data
+
+# User/Group
+useradd -m PyApi
+
+# Change Named Piped File Ownership + Permissions
+chown PyApi:PyApi data
+chmod 440 data
+
+# Import Python API
+API="test.py"
+
+# Change Python API Ownership + Permissions
+chown PyApi:PyApi $API
+chmod 110 $API
+
+# Move to PyApi 
+mv data /home/PyApi
+mv $API /home/PyApi
