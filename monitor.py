e0181d3c9c8e605221477afb7d9638b2dd425a94
stoffletd <156819575+stoffletd@users.noreply.github.com>
2025-07-14 03:14:49 -0700

    Update monitor.py
    All metric monitoring is included. Implemented monitor by system instead of by process.

--- monitor.py
+++ monitor.py
@@ -1,31 +1,23 @@
-3/5 Metrics Implemented so Far
-
 import psutil
 import time
-import subprocess
-# Need to Update PIDOF for More Command Variations
-stressPID = list(map(int, subprocess.check_output(["pidof", "stress-ng-hdd"]).decode().split()))
+import sys
 
-# Monitor Indefinately 
+# Named Pipe Output
+sys.stdout = open('/home/PyApi/data', 'w', buffering = 1)
+
 while True:
-    cpu = []
-    memory = []
-    io = []
-    net = []
-    disk = []
-    for pid in stressPID:
-        # Identify Specific Process
-        process = psutil.Process(pid)
-        # Sometimes Garbage Values are Recorded
-        # We Ignore These
-        if (process.cpu_percent(interval=1) != 0):
-            # Add Each Benchmark to the Lists According to PID
-            cpu.append(process.cpu_percent(interval=1))
-            memory.append(process.memory_percent())
-            io.append(process.io_counters()[3])
-    # Timer
-    time.sleep(3)
-    # Average Benchmarks of all Stress Processes
-    print("CPUdisk Percent Average: " + str(float(sum(cpu)/len(cpu))) + "%")
-    print("Memory Percent Average: " + str(float(sum(memory)/len(memory))) + "%")
-    print("I/O Write Average: " + str(float(sum(io)/len(io))) + " Bytes")
+    # Network Throughput (Data Out per Second)
+    start = psutil.net_io_counters()
+    time.sleep(1)
+    finish = psutil.net_io_counters()
+    # CPU
+    print("CPU Percent: " + str(psutil.cpu_percent(1)) + "%" )
+    # Memory
+    print("Memory Percent: " + str(psutil.virtual_memory().percent) + "%")
+    # Disk
+    print("Disk Usage: " + str(psutil.disk_usage('/').percent) + "%")
+    # IO
+    print("IO Write: " + str(psutil.disk_io_counters().write_bytes) + " Bytes")
+    print("IO Read: " + str(psutil.disk_io_counters().read_bytes) + " Bytes")
+    # Network
+    print("Network Throughput: " + str(finish.bytes_sent - start.bytes_sent) + " B/s")
